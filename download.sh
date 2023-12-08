f791d705dd3303f3c9f3af893b56cdd38d9f0d75
shangfuu <chenshungfu@gmail.com>
2023-12-08 15:49:15 +0800

    update download scripts

--- download.sh
+++
@@ -1,4 +0,0 @@
-python download_dataset.py
-
-cd data/
-unzip ISIC2018_dataset.tar
---
+++ scripts/download.sh
@@ -0,0 +1,9 @@
+mkdir ./data && cd ./data
+
+# download google drive file with id
+gdown 1bhcKPU2yeus4Kno9bI235anKj8wg9KTj
+gdown 1aFUeH4Lwtu9plt5MAJaZZGgE7RTfeP2x
+
+# unzip
+unzip ISIC2018_Dataset.zip
+unzip ISIC2019_Dataset.zip
